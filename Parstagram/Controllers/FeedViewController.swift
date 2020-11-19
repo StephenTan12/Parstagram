55a0af4f18cce58e3450047d28b5dfb9b911c8b9
Stephen Tan <sttan@ucsd.edu>
2020-11-19 15:54:24 -0800

    Users can logout

--- Parstagram/Controllers/FeedViewController.swift
+++ Parstagram/Controllers/FeedViewController.swift
@@ -35,6 +35,17 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         getPosts()
     }
     
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        
+        let sceneDelegate = self.view.window?.windowScene?.delegate as! SceneDelegate
+        
+        sceneDelegate.window?.rootViewController = loginViewController
+    }
+    
     @objc func onRefresh(_ refreshControl: UIRefreshControl) {
         getPosts()
         refreshControl.endRefreshing()
